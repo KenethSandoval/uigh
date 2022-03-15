612232c8495764533f49e169933f791b7adc23f9
KenethSandoval <kenethra.74@gmail.com>
2022-03-14 22:08:56 -0600

    feat: ui run

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/KenethSandoval/uigh/ui"
+)
+
+func main() {
+	if err := ui.NewProgram("stiv").Start(); err != nil {
+		fmt.Println("Could not start uigh", err)
+		os.Exit(1)
+	}
+}
--- ui/ui.go
+++ ui/ui.go
@@ -4,17 +4,21 @@ import (
 	"github.com/KenethSandoval/uigh/ui/common"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type status int
 
+const (
+	statusInit status = iota
+	statusLoading
+)
+
 type model struct {
 	quit     bool
-	done     bool
-	username string
 	spinner  spinner.Model
-	errorMsg string
 	status   status
+	username string
 }
 
 func NewProgram(username string) *tea.Program {
@@ -24,7 +28,7 @@ func NewProgram(username string) *tea.Program {
 func initialModel(username string) model {
 	return model{
 		username: username,
-		spinner:  common.NewSpinnerModel(),
+		status:   statusInit,
 	}
 }
 
@@ -32,6 +36,52 @@ func (m model) Init() tea.Cmd {
 	return spinner.Tick
 }
 
-func (m model) Update() {}
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			m.quit = true
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		// TODO: windows resizing
+	case spinner.TickMsg:
+		m.spinner, cmd = m.spinner.Update(msg)
+	}
+	var cmds []tea.Cmd
+	cmds = common.AppendIfNotNil(cmds, cmd)
+	m, cmd = updateChildre(m, msg)
+	cmds = common.AppendIfNotNil(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
 
-func (m model) View() {}
+func updateChildre(m model, msg tea.Msg) (model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch m.status {
+	case statusInit:
+	case statusLoading:
+		m.spinner, cmd = m.spinner.Update(msg)
+		cmd = tea.Batch(cmd, m.loadUserCmd)
+		m.status = statusLoading
+	}
+	return m, cmd
+}
+
+func (m model) View() string {
+	s := "KENETH"
+	switch m.status {
+	case statusInit:
+		s += " Loading user..."
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Top, s)
+}
+
+func (m model) loadUserCmd() tea.Msg {
+	if m.status == statusLoading {
+		return spinner.Tick()
+	}
+	return tea.ExitAltScreen()
+}
